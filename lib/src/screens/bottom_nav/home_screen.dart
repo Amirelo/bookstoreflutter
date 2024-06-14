389a947b4f065aaf2fb9d0b092bff177cb84e074
Amirelo <80700398+Amirelo@users.noreply.github.com>
2024-06-14 09:20:14 +0700

    Update CategoryCard and add animation

--- lib/src/screens/bottom_nav/home_screen.dart
+++ lib/src/screens/bottom_nav/home_screen.dart
@@ -1,10 +1,12 @@
 import 'package:bookstore/src/models/book_model.dart';
 import 'package:bookstore/src/models/category_model.dart';
 import 'package:bookstore/src/providers/category_provider.dart';
+import 'package:bookstore/src/widgets/custom_button.dart';
 import 'package:bookstore/src/widgets/custom_image.dart';
 import 'package:bookstore/src/widgets/custom_text.dart';
 import 'package:bookstore/src/widgets/product/card_product.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,7 +15,8 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
   List<CategoryModel> categoryList = List.empty();
 
   void getData() async {
@@ -37,6 +40,13 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    late final AnimationController animController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 2000));
+
+    late final Animation<double> testAnimation = Tween(begin: 0.0, end: 1.0)
+        .animate(
+            CurvedAnimation(parent: animController, curve: Curves.easeOut));
+
     const BookModel book1 = BookModel(
         "1",
         "Test book 1",
@@ -60,78 +70,104 @@ class _HomeScreenState extends State<HomeScreen> {
         "https://images.pexels.com/photos/16446088/pexels-photo-16446088/free-photo-of-colorful-cloths-over-house-door.jpeg");
     List<BookModel> bookList = <BookModel>[book1, book2, book3];
     bookList.add(book1);
+
     return (Scaffold(
-      body: Container(
-        width: const BoxConstraints().maxWidth,
-        padding: const EdgeInsets.only(top: 40, left: 16, right: 16),
-        child: Column(children: [
-          CustomImage(
-            imageLink:
-                "https://images.pexels.com/photos/16446088/pexels-photo-16446088/free-photo-of-colorful-cloths-over-house-door.jpeg",
-            width: const BoxConstraints().maxWidth,
-            height: 200,
-          ),
-          const Wrap(
-            spacing: 20,
-            crossAxisAlignment: WrapCrossAlignment.center,
-            children: [
-              Icon(
-                Icons.circle,
-                size: 16,
-              ),
-              Icon(Icons.circle, size: 10),
-              Icon(Icons.circle, size: 10),
-            ],
-          ),
-          const Align(
-            alignment: Alignment.topLeft,
-            child: CustomText(
-              title: "Categories",
-              paddingBottom: 8,
-              fontPreset: FontPresets.title,
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.only(bottom: 20),
+        child: Container(
+          width: const BoxConstraints().maxWidth,
+          padding: const EdgeInsets.only(top: 40, left: 16, right: 16),
+          child: Column(children: [
+            CustomButton(
+              title: "Rotate",
+              onPress: () => animController..forward(),
             ),
-          ),
-          SizedBox(
-            width: const BoxConstraints().maxWidth,
-            height: 200,
-            child: ListView.builder(
-              itemCount: categoryList.length,
-              itemBuilder: (context, index) => Container(
-                width: 100,
-                height: 40,
-                color: Colors.green,
-                child: CustomText(
-                  title: categoryList[index].name,
-                  fontPreset: FontPresets.title,
+            CustomImage(
+              imageLink:
+                  "https://images.pexels.com/photos/16446088/pexels-photo-16446088/free-photo-of-colorful-cloths-over-house-door.jpeg",
+              width: const BoxConstraints().maxWidth,
+              height: 200,
+            ),
+            const Wrap(
+              spacing: 20,
+              crossAxisAlignment: WrapCrossAlignment.center,
+              children: [
+                Icon(
+                  Icons.circle,
+                  size: 16,
+                ),
+                Icon(Icons.circle, size: 10),
+                Icon(Icons.circle, size: 10),
+              ],
+            ),
+            const Align(
+              alignment: Alignment.topLeft,
+              child: CustomText(
+                title: "Categories",
+                paddingBottom: 8,
+                fontPreset: FontPresets.title,
+              ),
+            ),
+            SizedBox(
+              width: const BoxConstraints().maxWidth,
+              height: 350,
+              child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemCount: categoryList.length > 5 ? 5 : categoryList.length,
+                separatorBuilder: (builder, index) => const SizedBox(width: 40),
+                itemBuilder: (context, index) => SizedBox(
+                  child: Column(children: [
+                    AnimatedBuilder(
+                      animation: testAnimation,
+                      builder: (_, child) {
+                        return Transform(
+                          transform: Matrix4.identity()
+                            ..setEntry(2, 2, 0.001)
+                            ..rotateY(animController.value * math.pi / 2),
+                          alignment: Alignment.center,
+                          child: child,
+                        );
+                      },
+                      child: CustomImage(
+                        imageLink: categoryList[index].image,
+                        width: 200,
+                        height: 300,
+                      ),
+                    ),
+                    CustomText(
+                      title: categoryList[index].name,
+                      fontPreset: FontPresets.title,
+                    ),
+                  ]),
                 ),
               ),
             ),
-          ),
-          const Align(
-            alignment: Alignment.topLeft,
-            child: CustomText(
-              title: "Bestsellers",
-              paddingBottom: 8,
-              fontPreset: FontPresets.title,
-            ),
-          ),
-          SizedBox(
-            width: const BoxConstraints().maxWidth,
-            height: 340,
-            child: ListView.separated(
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              itemCount: bookList.length,
-              itemBuilder: (BuildContext context, int index) {
-                return CardProduct(bookModel: bookList[index]);
-              },
-              separatorBuilder: (BuildContext context, int index) =>
-                  const SizedBox(
-                width: 12,
+            const Align(
+              alignment: Alignment.topLeft,
+              child: CustomText(
+                title: "Bestsellers",
+                paddingBottom: 8,
+                fontPreset: FontPresets.title,
               ),
             ),
-          ),
-        ]),
+            SizedBox(
+              width: const BoxConstraints().maxWidth,
+              height: 340,
+              child: ListView.separated(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: bookList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return CardProduct(bookModel: bookList[index]);
+                },
+                separatorBuilder: (BuildContext context, int index) =>
+                    const SizedBox(
+                  width: 12,
+                ),
+              ),
+            ),
+          ]),
+        ),
       ),
     ));
   }
